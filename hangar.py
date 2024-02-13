1dd6795dcdeb12d8e48e25aac01fe9830e1fe04a
Shashank M S <118469772+MS-crypto21@users.noreply.github.com>
2024-02-14 04:42:34 +0530

    made few changes and add new table

--- hangar.py
+++ hangar.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import sqlite3
 
+st.set_page_config(layout="wide")
+
 conn = sqlite3.connect("hangar.db")
 
-st.title("Airplanes Hangar Database Management System")
+st.title(":blue[Airplanes] ✈️ Hangar Database Management System", anchor=False)
 
+st.divider()
 # Creating a form for the airplane table
 with st.form(key="airplane_form", clear_on_submit=False):
-    st.subheader("Add a new airplane")
+    st.header(":violet[Add a new airplane]", anchor=False, divider="rainbow")
     reg_no = st.number_input("Registration Number *", min_value=1)
     model = st.text_input("Model *")
     manufacturer = st.text_input("Manufacturer *")
@@ -30,76 +33,88 @@ if airplane_submit_button:
     except sqlite3.IntegrityError:
         st.warning("Error: Registration number already exists.")
 
+st.divider()
+
+col1, col2, = st.columns(2)
+
 # Creating a form for the Hangar
-with st.form(key='hangar_form', clear_on_submit=False):
-    st.subheader("Add a new Hangar")
-    hangar_id = st.text_input("Hangar ID *")
-    status = st.selectbox('Status *', ("Active", "Inactive"), index=None, placeholder="---SELECT ANY OF THE CHOICES---")
-    capacity = st.text_input("Capacity *")
+with st.container():
+    with col1:
+        with st.form(key='hangar_form', clear_on_submit=False):
+            st.header("Add a new Hangar", anchor=False, divider="rainbow")
+            hangar_id = st.text_input("Hangar ID *")
+            status = st.selectbox('Status *', ("Active", "Inactive"), index=None,
+                                  placeholder="---SELECT ANY OF THE CHOICES---")
+            capacity = st.text_input("Capacity *")
 
-    # Query the database for existing airplane registration numbers
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    cursor.execute("SELECT reg_no FROM airplane")
-    rows = cursor.fetchall()
-    reg_nos = [row['reg_no'] for row in rows]
-    reg_no = st.selectbox('Airplane Registration Number *', reg_nos)
+            # Query the database for existing airplane registration numbers
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            cursor.execute("SELECT reg_no FROM airplane")
+            rows = cursor.fetchall()
+            reg_nos = [row['reg_no'] for row in rows]
+            reg_no = st.selectbox('Airplane Registration Number *', reg_nos)
 
-    # If the reg_no is already present in the hangar, I should prevent it from further using since its already in use
-    # cursor.execute("SELECT reg_no FROM hanger")
-    # column = cursor.fetchall()
-    # column_nos = [column['reg_no'] for columns in column]
+            # If the reg_no is already present in the hangar, I should prevent it from further
+            # using since its already in use
+            # cursor.execute("SELECT reg_no FROM hanger")
+            # column = cursor.fetchall()
+            # column_nos = [column['reg_no'] for columns in column]
 
-    hangar_submit_button = st.form_submit_button(label="Insert")
+            hangar_submit_button = st.form_submit_button(label="Insert")
 
-if hangar_submit_button:
-    try:
-        if not (hangar_id and status and capacity and reg_no):
-            st.warning("Please fill all the fields before Submitting")
-        else:
-            conn.execute(f"""
-            INSERT INTO hanger (hanger_id, status, capacity, reg_no) 
-            VALUES ('{hangar_id}', '{status}', '{capacity}', {reg_no})
-            """)
-            conn.commit()
-            st.success("Data has been successfully inserted!", icon='✅')
-    except sqlite3.IntegrityError:
-        st.warning("Error: Hangar ID already exists.")
+        if hangar_submit_button:
+            try:
+                if not (hangar_id and status and capacity and reg_no):
+                    st.warning("Please fill all the fields before Submitting")
+                else:
+                    conn.execute(f"""
+                    INSERT INTO hanger (hanger_id, status, capacity, reg_no) 
+                    VALUES ('{hangar_id}', '{status}', '{capacity}', {reg_no})
+                    """)
+                    conn.commit()
+                    st.success("Data has been successfully inserted!", icon='✅')
+            except sqlite3.IntegrityError:
+                st.warning("Error: Hangar ID already exists.")
 
-with st.form(key='location_form', clear_on_submit=False):
-    st.subheader("Add the location Sector and direction")
-    sector = st.selectbox("Sector *", ("A", "B", "C", "D"), index=None, placeholder="---SELECT THE SECTOR---")
-    direction = st.selectbox("Directions *", ("North", "South", "East", "West",
-                                              "North-East (NE)", "South-East (SE)", "South-West (SW)",
-                                              "North-West(NW)"), index=None,
-                             placeholder="---SELECT THE DIRECTION TO THE SECTOR---")
+    with col2:
+        with st.form(key='location_form', clear_on_submit=False):
+            st.header("Add the location Sector and direction", anchor=False, divider="rainbow")
+            sector = st.selectbox("Sector *", ("A", "B", "C", "D"), index=None, placeholder="---SELECT THE SECTOR---")
+            direction = st.selectbox("Directions *", ("North", "South", "East", "West",
+                                                      "North-East (NE)", "South-East (SE)", "South-West (SW)",
+                                                      "North-West(NW)"), index=None,
+                                     placeholder="---SELECT THE DIRECTION TO THE SECTOR---")
 
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    cursor.execute("SELECT hanger_id FROM hanger")
-    hanger_row = cursor.fetchall()
-    hangar_nos = [row['hanger_id'] for row in hanger_row]
-    hangar_no = st.selectbox("Airplane Hangar ID *", hangar_nos)
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            cursor.execute("SELECT hanger_id FROM hanger")
+            hanger_row = cursor.fetchall()
+            hangar_nos = [row['hanger_id'] for row in hanger_row]
+            hangar_no = st.selectbox("Airplane Hangar ID *", hangar_nos)
 
-    location_submit_button = st.form_submit_button(label='Insert')
+            location_submit_button = st.form_submit_button(label='Insert')
 
-if location_submit_button:
-    try:
-        if not (sector and direction and hangar_no):
-            st.warning("Please fill all the fields")
-        else:
-            conn.execute(f"""
-            INSERT INTO hanger_location (sector, direction, hanger_id)
-            VALUES ('{sector}', '{direction}', '{hangar_no}')
-            """)
-            conn.commit()
-            st.success("Data has been successfully inserted!", icon='✅')
-    except sqlite3.IntegrityError:
-        st.warning("Error: Hangar_ID, Sector and Direction already exists")
+        if location_submit_button:
+            try:
+                if not (sector and direction and hangar_no):
+                    st.warning("Please fill all the fields")
+                else:
+                    conn.execute(f"""
+                    INSERT INTO hanger_location (sector, direction, hanger_id)
+                    VALUES ('{sector}', '{direction}', '{hangar_no}')
+                    """)
+                    conn.commit()
+                    st.success("Data has been successfully inserted!", icon='✅')
+            except sqlite3.IntegrityError:
+                st.warning("Error: Hangar_ID, Sector and Direction already exists")
+
+
+st.divider()
 
 # Creating a form for personal information
 with st.form(key='personal_form', clear_on_submit=False):
-    st.subheader("Add a personal information")
+    st.header("Add a personal information", anchor=False, divider="rainbow")
     emp_id = st.number_input("Employee ID *", min_value=1000)
     name = st.text_input("Name *")
     position = st.selectbox("Employee Position *", ("Worker", 'Hangar Manager', 'Quality Control Manager',
@@ -107,6 +122,7 @@ with st.form(key='personal_form', clear_on_submit=False):
                                                     'Inventory Manager', 'Director of Product Management'),
                             index=None,
                             placeholder="---SELECT YOUR POSITION---")
+
 
     personal_submit_button = st.form_submit_button(label="Insert")
 
@@ -123,3 +139,25 @@ if personal_submit_button:
             st.success("Data has been successfully inserted!", icon='✅')
     except sqlite3.IntegrityError:
         st.warning("Employee ID already exists")
+
+st.divider()
+
+# with st.form(key='', clear_on_submit=False):
+with st.form(key='Workers_In_Hangar', clear_on_submit=False):
+    st.header("Workers in Hangar", anchor=False, divider="rainbow")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute("SELECT hanger_ID FROM hanger")
+    hanger_row = cursor.fetchall()
+    hanger_Id_Nos = [row['hanger_ID'] for row in hanger_row]
+
+    foreign_key_hangarId = st.selectbox("Hangar's ID ", hanger_Id_Nos)
+
+    cursor.execute("SELECT emp_id FROM personnel")
+    emp_row = cursor.fetchall()
+    emp_rows = [row['emp_id'] for row in emp_row]
+
+    foreign_key_empId = st.selectbox("Employee's ID", emp_rows)
+
+    working_submit_button = st.form_submit_button(label="Enter")
+
